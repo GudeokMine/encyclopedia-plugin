b39e03db12a9fafc1b68d1b5036036b2ba5588a5
sebanimm <gks32114221@gmail.com>
2024-10-27 14:14:48 +0900

    명령어로 GUI를 보여주도록 변경

--- src/main/kotlin/io/github/lepitar/encyclopedia/plugin/EncyclopediaPlugin.kt
+++ src/main/kotlin/io/github/lepitar/encyclopedia/plugin/EncyclopediaPlugin.kt
@@ -1,46 +1,21 @@
 package io.github.lepitar.encyclopedia.plugin
 
-import io.github.lepitar.encyclopedia.plugin.gui.EncyclopediaGUI
+import io.github.lepitar.encyclopedia.plugin.command.EncyclopediaCommand
 import io.github.lepitar.encyclopedia.plugin.item.ItemCollectionManager
-import org.bukkit.Material
-import org.bukkit.event.EventHandler
-import org.bukkit.event.Listener
-import org.bukkit.event.block.Action
-import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.plugin.java.JavaPlugin
 
 
-class EncyclopediaPlugin : JavaPlugin(), Listener {
+class EncyclopediaPlugin : JavaPlugin() {
     private lateinit var collectionManager: ItemCollectionManager
 
     override fun onEnable() {
         collectionManager = ItemCollectionManager(this)
         collectionManager.loadCollections()
-
-        server.pluginManager.registerEvents(this, this)
+        
+        getCommand("도감")?.setExecutor(EncyclopediaCommand(collectionManager))
     }
 
     override fun onDisable() {
         collectionManager.saveCollections()
-    }
-
-    @EventHandler
-    fun onPlayerInteract(event: PlayerInteractEvent) {
-        if (event.action != Action.RIGHT_CLICK_AIR && event.action != Action.RIGHT_CLICK_BLOCK) {
-            return
-        }
-
-        val item = event.item ?: return
-        if (item.type != Material.BOOK) {
-            return
-        }
-
-        val player = event.player
-        EncyclopediaGUI.Builder("도감")
-            .asItem(Material::isItem)
-            .forPlayer(player)
-            .withCollectionManager(collectionManager)
-            .build()
-            .show(player)
     }
 }
---
+++ src/main/kotlin/io/github/lepitar/encyclopedia/plugin/command/EncyclopediaCommand.kt
@@ -0,0 +1,27 @@
+package io.github.lepitar.encyclopedia.plugin.command
+
+import io.github.lepitar.encyclopedia.plugin.gui.EncyclopediaGUI
+import io.github.lepitar.encyclopedia.plugin.item.ItemCollectionManager
+import org.bukkit.Material
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+class EncyclopediaCommand(private val collectionManager: ItemCollectionManager) : CommandExecutor {
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        if (sender !is Player) {
+            sender.sendMessage("This command can only be used by players.")
+            return false
+        }
+
+        EncyclopediaGUI.Builder("도감")
+            .asItem(Material::isItem)
+            .forPlayer(sender)
+            .withCollectionManager(collectionManager)
+            .build()
+            .show(sender)
+
+        return true
+    }
+}
